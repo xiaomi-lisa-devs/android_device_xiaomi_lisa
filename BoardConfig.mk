8a6519a4cd96723fdb8ecc968a573c0bb0eec841
ComradeWave <94688733+ComradeWave@users.noreply.github.com>
2024-07-18 16:09:14 +0200

    Adapt for Matrixx and fix for warning TARGET_BOOTLOADER_RES
    Added required flags to build a Matrixx rom and fixed warning of TARGET_BOOTLOADER_RES

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,19 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := lisa
 
+# Matrixx
+MATRIXX_CHIPSET := SM8350
+MATRIXX_MAINTAINER := ComradeWave
+MATRIXX_BATTERY := 4250mah
+MATRIXX_DISPLAY := 1080x2400
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+WITH_GMS := true
+
+# Bootloader
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/lisa_QGKI.config
 
